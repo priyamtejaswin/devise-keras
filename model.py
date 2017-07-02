bc6ae64b16883fe0d4baf8ff8126f6fd25e6b8a4
Priyam Tejaswin <priyamtejaswin@gmail.com>
2017-07-02 19:34:26 +0530

    Updated model.py - THIS IS JUST FOR REFERENCE. The fill will be deleted once I updated rnn_model.py with the correct loss function and callbacks.

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from keras.models import Model
 from keras.layers import Input
 from keras.layers import Dense
 from keras.layers import Lambda
+from keras.layers.normalization import BatchNormalization
 from keras.callbacks import RemoteMonitor
 import keras
 from time import time, sleep
@@ -13,10 +14,11 @@ from extract_features_and_dump import data_generator
 import numpy as np
 from keras.callbacks import TensorBoard
 import cv2
+from validation_script import ValidCallBack
 
 PATH_h5 = "processed_features/features.h5"
-MARGIN = 0.5
-INCORRECT_BATCH = 4
+MARGIN = 0.2
+INCORRECT_BATCH = 32
 BATCH = INCORRECT_BATCH + 1
 IMAGE_DIM = 4096
 WORD_DIM = 50
@@ -26,7 +28,7 @@ class DelayCallback(keras.callbacks.Callback):
 		pass
 
 	def on_batch_end(self, batch, logs={}):
-		sleep(0.01)
+		sleep(0.05)
 
 class EpochCheckpoint(keras.callbacks.Callback):
 	def __init__(self, folder):
@@ -57,7 +59,8 @@ def linear_transformation(a):
 	Takes a 4096-dim vector, applies linear transformation to get WORD_DIM vector.
 	"""
 	b = Dense(WORD_DIM, name='transform')(a)
-	return b
+	c = BatchNormalization()(b)
+	return c
 
 def hinge_rank_loss(word_vectors, image_vectors, TESTING=False):
 	"""
@@ -77,7 +80,7 @@ def hinge_rank_loss(word_vectors, image_vectors, TESTING=False):
 	wrong_words = Lambda(slice_but_first, output_shape=(INCORRECT_BATCH, WORD_DIM))(word_vectors)
 
 	# l2 norm
-	l2 = lambda x: K.sqrt(K.sum(K.square(x)))
+	l2 = lambda x: K.sqrt(K.sum(K.square(x), axis=1, keepdims=True))
 	l2norm = lambda x: x/l2(x)
 
 	# tiling to replicate correct_word and correct_image
@@ -91,15 +94,15 @@ def hinge_rank_loss(word_vectors, image_vectors, TESTING=False):
 	wrong_images = l2norm(wrong_images)
 
 	# correct_image VS incorrect_words | Note the singular/plurals
-	cost_images = MARGIN - K.sum(correct_images * correct_words, 1) + K.sum(correct_images * wrong_words, 1) 
-	cost_images = K.maximum(cost_images, 0.0)
+	# cost_images = MARGIN - K.sum(correct_images * correct_words, 1) + K.sum(correct_images * wrong_words, 1) 
+	# cost_images = K.maximum(cost_images, 0.0)
 	
 	# correct_word VS incorrect_images | Note the singular/plurals
-	cost_words = MARGIN - K.sum(correct_words * correct_images, 1) + K.sum(correct_words * wrong_images, 1) 
+	cost_words = MARGIN - K.sum(correct_words * correct_images, axis=1) + K.sum(correct_words * wrong_images, axis=1) 
 	cost_words = K.maximum(cost_words, 0.0)
 
 	# currently cost_words and cost_images are vectors - need to convert to scalar
-	cost_images = K.sum(cost_images, axis=-1)
+	# cost_images = K.sum(cost_images, axis=-1)
 	cost_words  = K.sum(cost_words, axis=-1)
 
 	if TESTING:
@@ -112,14 +115,15 @@ def hinge_rank_loss(word_vectors, image_vectors, TESTING=False):
 		assert K.eval(wrong_words).shape==K.eval(wrong_images).shape
 		assert K.eval(correct_words).shape==K.eval(wrong_images).shape
 	
-	return cost_words + cost_images
+	# return cost_words + cost_images
+	return cost_words/INCORRECT_BATCH
 	
 
 def build_model(image_features, word_features=None):
 	image_vector = linear_transformation(image_features)
 
 	mymodel = Model(inputs=image_features, outputs=image_vector)
-	mymodel.compile(optimizer="adagrad", loss=hinge_rank_loss)
+	mymodel.compile(optimizer="adam", loss=hinge_rank_loss)
 	return mymodel
 
 def main():
@@ -138,18 +142,18 @@ def main():
 		# remote_cb = RemoteMonitor(root='http://localhost:9000')
 		tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
 		epoch_cb    = EpochCheckpoint(folder="./snapshots/")
-		delay_cb 	= DelayCallback()
+		valid_cb	= ValidCallBack()
 
 		# fit generator
 		steps_per_epoch = math.ceil(_num_train/float(BATCH))
 		print "Steps per epoch i.e number of iterations: ",steps_per_epoch
 		
-		train_datagen = data_generator(batch_size=INCORRECT_BATCH)
+		train_datagen = data_generator(batch_size=INCORRECT_BATCH, image_class_ranges="training_ranges.pkl")
 		history = model.fit_generator(
 				train_datagen,
 				steps_per_epoch=steps_per_epoch,
-				epochs=50,
-				callbacks=[tensorboard, delay_cb, epoch_cb]
+				epochs=250,
+				callbacks=[tensorboard, valid_cb]
 			)
 		print history.history.keys()
 
@@ -157,47 +161,6 @@ def main():
 	elif RUN_TIME == "TEST":
 		from keras.models import load_model 
 		model = load_model("snapshots/epoch_49.hdf5", custom_objects={"hinge_rank_loss":hinge_rank_loss})
-
-	# predict on some sample images
-	from extract_features_and_dump import define_model
-	vgg16 = define_model(path="./vgg16_weights_th_dim_ordering_th_kernels.h5")
-
-	# load word embeddings and word names 
-	hf = h5py.File("processed_features/features.h5","r")
-	v_h5 = hf["data/word_embeddings"]
-	w_h5 = hf["data/word_names"]
-	v_h5 = v_h5[:,:]
-	v_h5 = v_h5 / np.linalg.norm(v_h5, axis=1, keepdims=True)
-	w_h5 = w_h5[:,:]
-
-	list_ims = ["./UIUC_PASCAL_DATA_clean/aeroplane/2008_000716.jpg",
-				"./UIUC_PASCAL_DATA_clean/bicycle/2008_000725.jpg",
-				"./UIUC_PASCAL_DATA_clean/bird/2008_008490.jpg"]
-
-	
-	for imname in list_ims:
-		
-		print "Running for image type: ",imname.split("/")[-2]
-
-		img = cv2.imread(imname)
-		# cv2.imshow("input",img); cv2.waitKey(0)
-		img = np.rollaxis(img, 2)
-		img = np.expand_dims(img, 0)
-		img_feats = vgg16.predict(img)
-		image_vec = model.predict(img_feats)
-		# print image_vec.shape
-		image_vec = image_vec / np.linalg.norm(image_vec)
-
-		diff = v_h5 - image_vec
-		diff = np.linalg.norm(diff, axis=1)
-		
-		bicycle_idx 	= np.where(w_h5==["bicycle"])[0]
-		aeroplane_idx 	= np.where(w_h5==["aeroplane"])[0]
-		bird_idx 		= np.where(w_h5==["bird"])[0]
-
-		print "bicycle: ", diff[bicycle_idx]
-		print "aeroplane: ", diff[aeroplane_idx]
-		print "bird: ", diff[bird_idx]
 
 	K.clear_session()
 
