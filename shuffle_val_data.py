f5b1262bfbeecfc1a332ef843053b827cea0b040
Akshay Chawla <chawla.akshay1234@gmail.com>
2017-07-04 22:53:05 +0530

    Fixed issue with shuffle_val_data.py

--- shuffle_val_data.py
+++ shuffle_val_data.py
@@ -5,6 +5,7 @@ config = ConfigParser.RawConfigParser()
 config.read('local.cfg')
 TRAINING_DATA_FOLDER 	= config.get("data location", "TRAINING_DATA_FOLDER")
 VALIDATION_DATA_FOLDER  = config.get("data location", "VALIDATION_DATA_FOLDER")
+print "TRAINING_DATA_FOLDER: ", TRAINING_DATA_FOLDER, " VALIDATION_DATA_FOLDER: ", VALIDATION_DATA_FOLDER
 
 count = 1
 
@@ -19,8 +20,8 @@ for root, dirs, files in os.walk(TRAINING_DATA_FOLDER):
 			files_to_copy.append(os.path.join(root, fname))
 			print root, fname
 
-			# new_root = root.replace("_DATA", "_VAL")
-			new_root   = VALIDATION_DATA_FOLDER
+			new_root = root.replace("_DATA", "_VAL")
+			# new_root   = VALIDATION_DATA_FOLDER
 			if not os.path.isdir(new_root):
 				os.mkdir(new_root)
 
