bba68aee06ceba9f47fcc75c0281a8c8ef94651c
Akshay Chawla <chawla.akshay1234@gmail.com>
2017-08-04 14:12:07 +0530

    Finished views.py, it now returns the fnames of top_k_indices that are closest in embedding space to given query_string. NOTE: THIS CODE IS UNTESTED, HAVE TO TEST IT TOMORROW.

--- views.py
+++ views.py
@@ -5,6 +5,7 @@ from keras.models import load_model
 from keras.preprocessing.text import text_to_word_sequence
 import os, sys
 import pickle
+import h5py
 from flask import Flask
 app = Flask(__name__)
 
@@ -17,15 +18,26 @@ MAX_SEQUENCE_LENGTH = 20
 MODEL=None
 DICT_word_index = None
 if DUMMY_MODE==False:
+	
 	MODEL = load_model(MODEL_LOC)
 	print MODEL.summary()
+	
 	assert os.path.isfile("DICT_word_index.TRAIN.pkl"), "Could not find DICT_word_index.TRAIN.pkl"	
+	
 	with open("DICT_word_index.TRAIN.pkl","r") as f:
 		DICT_word_index = pickle.load(f)
 	assert DICT_word_index is not None, "Could not load dictionary that maps word to index"
 
+	im_outs = None 
+	fnames = None
+	with h5py.File("cache.h5") as F:
+		im_outs = F["data/im_outs"][:]
+		fnames  = F["data/fnames"][:]
+	assert im_outs is not None, "Could not load im_outs from cache.h5"
+	assert fnames is not None, "Could not load fnames from cache.h5"
+
 # Query string -> word index list 
-def processing_query_string(query_string):
+def query_string_to_word_indices(query_string):
 	
 	# string -> list of words 
 	words = text_to_word_sequence(
@@ -73,9 +85,22 @@ def run_model(query_string):
 			result = ["static/dog.jpg", "static/dog.jpg", "static/dog.jpg"]
 		else:
 			assert MODEL is not None, "not in dummy mode, but model did not load!"
-			# do something 
+			
+			# forward pass 
+			output = MODEL.predict([ np.zeros(1,4096) , query_string_to_word_indices(query_string) ])[:, WORD_DIM: ]
+			output = output / np.linalg.norm(output, axis=1, keepdims=True)
+			
+			# compare with im_outs
+			TOP_K = 50
+			diff = im_outs - output 
+			diff = np.linalg.norm(diff, axis=1)
+			top_k_indices = np.argsort(diff)[:TOP_K].tolist()
 
+			# populate "results" with fnames of top_k_indices
 			result = []
+			for k in top_k_indices:
+				result.append(fnames[k])
+			
 		print '..over'
 	
 	if result is None:
