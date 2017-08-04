10019ce3bfe63b97e97eda862e50623f53aff6ba
Akshay Chawla <chawla.akshay1234@gmail.com>
2017-08-04 13:06:32 +0530

    changes to views.py 1. load up DICT_word_index that contains mapping from string word to its index. 2. fun preprocessing_query_string: uses (1) to convert string to list of word indices.

--- views.py
+++ views.py
@@ -2,17 +2,48 @@ import time
 from flask import render_template, jsonify, request
 from multiprocessing import Lock
 from keras.models import load_model
-
+from keras.preprocessing.text import text_to_word_sequence
+import os, sys
+import pickle
 from flask import Flask
 app = Flask(__name__)
 
 DUMMY_MODE=True
 MODEL_LOC="snapshots/epoch_x.hdf5"
 
+# VERY IMPORTANT VARIABLES
 mutex = Lock()
 MODEL=None
+DICT_word_index = None
 if DUMMY_MODE==False:
 	MODEL = load_model(MODEL_LOC)
+	assert os.path.isfile("DICT_word_index.TRAIN.pkl"), "Could not find DICT_word_index.TRAIN.pkl"	
+	with open("DICT_word_index.TRAIN.pkl","r") as f:
+		DICT_word_index = pickle.load(f)
+	assert DICT_word_index is not None, "Could not load dictionary that maps word to index"
+
+# Query string -> word index list 
+def processing_query_string(query_string):
+	
+	# string -> list of words 
+	words = text_to_word_sequence(
+			text = query_string,
+			filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+			lower=True,
+			split=" "
+		)
+
+	# check if words in dictionary
+	all_words = DICT_word_index.keys()
+	for word in words:
+		assert word in all_words, "could not find word {} in all_words".format(word)
+
+	# list of words -> list of indices
+	words_index = []
+	for word in words:
+		words_index.append(DICT_word_index[word])
+
+	return words_index
 
 @app.route("/")
 @app.route("/index")
