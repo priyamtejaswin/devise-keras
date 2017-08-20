5e1fac16eacc4f403b84a858899e92b4490e4355
Priyam Tejaswin <priyamtejaswin@gmail.com>
2017-08-20 18:39:38 +0530

    complete_model.py | Added the distance function.

--- complete_model.py
+++ complete_model.py
@@ -1,15 +1,78 @@
 import numpy as np
 import pickle 
 from extract_features_and_dump import define_model
-from rnn_model import hinge_rank_loss
+# from rnn_model import hinge_rank_loss
 import keras 
 from keras.models import Model, load_model 
 from keras.layers import Input, Dense, BatchNormalization, Activation, Dropout, Embedding, LSTM, concatenate
 import keras.backend as K
+from keras.layers import Lambda
 
 # globals 
 MAX_SEQUENCE_LENGTH = 20
 WORD_DIM=300 
+INCORRECT_BATCH=0
+BATCH = INCORRECT_BATCH + 1
+MARGIN = 0.2
+IMAGE_DIM = 4096
+
+def hinge_rank_loss(y_true, y_pred, TESTING=False):
+	"""
+	Custom hinge loss per (image, label) example - Page4.
+	
+	Keras mandates the function signature to follow (y_true, y_pred)
+	In devise:master model.py, this function accepts:
+	- y_true as word_vectors
+	- y_pred as image_vectors
+
+	For the rnn_model, the image_vectors and the caption_vectors are concatenated.
+	This is due to checks that Keras has enforced on (input,target) sizes 
+	and the inability to handle multiple outputs in a single loss function.
+
+	These are the actual inputs to this function:
+	- y_true is just a dummy placeholder of zeros (matching size check)
+	- y_pred is concatenate([image_output, caption_output], axis=-1)
+	The image, caption features are first separated and then used.
+	"""
+	## y_true will be zeros
+	select_images = lambda x: x[:, :WORD_DIM]
+	select_words = lambda x: x[:, WORD_DIM:]
+
+	slice_first = lambda x: x[0:1 , :]
+
+	# separate the images from the captions==words
+	image_vectors = Lambda(select_images, output_shape=(BATCH, WORD_DIM))(y_pred)
+	word_vectors = Lambda(select_words, output_shape=(BATCH, WORD_DIM))(y_pred)
+
+	# separate correct/wrong images
+	correct_image = Lambda(slice_first, output_shape=(1, WORD_DIM))(image_vectors)
+
+	# separate correct/wrong words
+	correct_word = Lambda(slice_first, output_shape=(1, WORD_DIM))(word_vectors)
+
+	# l2 norm
+	l2 = lambda x: K.sqrt(K.sum(K.square(x), axis=1, keepdims=True))
+	l2norm = lambda x: x/l2(x)
+	
+	correct_image = l2norm(correct_image)
+	correct_word = l2norm(correct_word)
+
+	# correct_image VS incorrect_words | Note the singular/plurals
+	cost_images = K.sum(correct_image * correct_word, axis=1)
+	cost_images = K.sum(cost_images, axis=-1)
+
+	if TESTING:
+		import ipdb
+		ipdb.set_trace()
+		assert K.eval(wrong_words).shape[0] == INCORRECT_BATCH
+		assert K.eval(correct_words).shape[0] == INCORRECT_BATCH
+		assert K.eval(wrong_images).shape[0] == INCORRECT_BATCH
+		assert K.eval(correct_images).shape[0] == INCORRECT_BATCH
+		assert K.eval(correct_words).shape==K.eval(correct_images).shape
+		assert K.eval(wrong_words).shape==K.eval(wrong_images).shape
+		assert K.eval(correct_words).shape==K.eval(wrong_images).shape
+	
+	return cost_images ## WAS EARLIER DIVIDING BY INCORRECT_BATCH WHICH HAS BEEN SET TO 0, IDIOT.
 
 def build_model(image_features, caption_features, embedding_matrix):
 	
@@ -50,7 +113,7 @@ def get_full_model(vgg_wts_path, rnn_wts_path):
 	vgg              = define_model(vgg_wts_path) # get image->4096 model
 	
 	caption_features = Input(shape=(MAX_SEQUENCE_LENGTH,), name="caption_feature_input") # the caption input 
-	embedding_matrix = pickle.load(open("KERAS_embedding_layer.TRAIN.pkl"))
+	embedding_matrix = pickle.load(open("/Users/tejaswin.p/projects/devise-keras/KERAS_embedding_layer.TRAIN.pkl"))
 		
 	full_model       = build_model(vgg.output, caption_features, embedding_matrix=embedding_matrix) # caption+4096Feats -> loss function 
 	
@@ -59,7 +122,7 @@ def get_full_model(vgg_wts_path, rnn_wts_path):
 
 	completeModel    = Model(inputs=[vgg.input, caption_features], outputs=full_model)
 	completeModel.load_weights(rnn_wts_path, by_name=True) # load up the rnn part weights
-
+	completeModel.compile("rmsprop", hinge_rank_loss)
 	return completeModel
 
 
@@ -69,4 +132,8 @@ def TEST():
 	K.clear_session()
 
 if __name__ == '__main__':
-	TEST()
+	# TEST()
+
+	y_true = K.variable(np.zeros(1))
+	y_pred = K.variable(np.random.rand(1, 600))
+	print K.eval(hinge_rank_loss(y_true, y_pred, TESTING=False))
