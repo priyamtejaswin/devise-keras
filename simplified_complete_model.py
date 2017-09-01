df1c56a498c3713f8d6305a651ba5d701c136862
Priyam Tejaswin <priyamtejaswin@gmail.com>
2017-09-01 22:23:39 +0530

    simplified_complete_model.py 1. Updated the VGG_model. Now loads from "extraction" module. 2. CORRECT_similarity==0.402, RANDOM_similarity==-0.491

--- simplified_complete_model.py
+++ simplified_complete_model.py
@@ -11,7 +11,7 @@ import sys, os
 from keras.applications import VGG16
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
-
+from extract_features_and_dump import define_model as TheVGGModel
 
 class FullModel:
 	
@@ -27,8 +27,9 @@ class FullModel:
 		assert os.path.isfile(rnn_model_loc), "Provided incorect location to rnn model"
 
 		# vgg16 - fc2 output
-		top_model = VGG16(weights="imagenet", include_top="True")
-		self.top_model = Model(inputs=top_model.input, outputs=top_model.get_layer("fc2").output)
+		# top_model = VGG16(weights="imagenet", include_top="True")
+		# self.top_model = Model(inputs=top_model.input, outputs=top_model.get_layer("fc2").output)
+		self.top_model = TheVGGModel("/Users/tejaswin.p/projects/devise-keras/vgg16_weights_th_dim_ordering_th_kernels.h5")
 
 		# rnn part + recompile with new loss
 		self.rnn_model = load_model(rnn_model_loc, custom_objects={"hinge_rank_loss":hinge_rank_loss})
@@ -60,32 +61,60 @@ class FullModel:
 
 		# correct_image VS incorrect_words | Note the singular/plurals
 		cost_images = K.sum(correct_image * correct_word, axis=1)
-		cost_images = K.sum(cost_images, axis=-1)
+		# cost_images = K.sum(cost_images, axis=-1) ## Commented because values expected for multiple inputs.
 
 		return cost_images ## WAS EARLIER DIVIDING BY INCORRECT_self.BATCH WHICH HAS BEEN SET TO 0, IDIOT.
 
 	def predict(self, x):
 
 		# checks
-		assert x.shape == (1, 3, 224, 224), "Incorrect input dims of {}".format(x.shape) 
+		# assert x.shape == (1, 3, 224, 224), "Incorrect input dims of {}".format(x.shape) 
+		## Change from (10, 224, 224, 3) to (10, 3, 224, 224)
+
+		# x_rolled = np.moveaxis(np.moveaxis(x, 3, 1), 2, 3)
+		
+		x_rolled = x
+		print x_rolled.shape
 
 		# pass through vgg 
-		top_op = self.top_model.predict(x)
+		top_op = self.top_model.predict(x_rolled)
 
 		# pass through rnn model (get loss) 
-		loss = self.rnn_model.test_on_batch([top_op, self.caption], np.zeros(1))
+		loss = self.rnn_model.test_on_batch( 
+			[top_op, np.tile(self.caption, (x_rolled.shape[0], 1))], 
+			np.zeros(x_rolled.shape[0]) )
+
+		# import ipdb
+		# ipdb.set_trace()
+		print loss.shape, loss
 
 		return loss
+
+	@staticmethod
+	def preprocess_image(img_path):
+		img_input = image.load_img(img_path, target_size=(224, 224))
+		x = image.img_to_array(img_input)
+		x = np.expand_dims(x, axis=0)
+		x = preprocess_input(x)
+		return x	
 
 	
 def TEST():
 	cap_input = np.array([[8, 214, 23, 1, 626, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-	img_input = image.load_img("/Users/tejaswin.p/Downloads/eating_pizza_in_park.jpg", target_size=(224, 224))
-	x = image.img_to_array(img_input)
-	x = np.expand_dims(x, axis=0)
-	x = preprocess_input(x)
     
 	model = FullModel(cap_input, "/Users/tejaswin.p/Downloads/epoch_9.hdf5")
+
+	# import ipdb
+	# ipdb.set_trace()
+
+	x = model.preprocess_image("/Users/tejaswin.p/Downloads/eating_pizza_in_park.jpg")
+	x = x.astype(np.float64)
+	print x.shape
+
+	# x = np.moveaxis(np.moveaxis(x, 1, 0), 2, 1)
+	# x = np.expand_dims(x, axis=0)
+	# print x.shape
+
 	print "CORRECT caption similarity:", model.predict(x)
 
 	model.caption = np.array([[8, 199, 23, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
