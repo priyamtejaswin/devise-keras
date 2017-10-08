99d89a8d83f63cd9b2efd6faa7a063fff6cd5ff4
Akshay Chawla <chawla.akshay1234@gmail.com>
2017-10-08 01:19:30 -0400

    Changes to be committed: 	new file:   gpu0.sh 	new file:   gpu1.sh 	new file:   gpu2.sh 	modified:   lime_results_dbase.db
    1. We ran lime for all queries present in gpu0.sh, gpu1.sh and gpu2.sh
    2. Updated lime_results_dbase.db
    3. lime images have been compressed to all_images_updated.tar.gz (which I will send via email soon) 1.3 MB
    
    <TODO>
    1. Create landing page without lime results (just devise image search engine)
    2. Use canvas api in html to have better overlays of lime results on images
    3. In order for (2) to happen, we need to update views.py so that it sends contours instead of mask images.
    4. Rename views.py appropriately, server_devise.py and server_deviselime.py

---
+++ gpu0.sh
@@ -0,0 +1,10 @@
+python cache_lime.py --word_index=/media/drive1/cwa5kor/personal/devise_cache/DICT_word_index.VAL.pkl --model=/media/drive1/cwa5kor/personal/devise_cache/epoch_13.hdf5 --vgg16=/media/drive1/cwa5kor/personal/devise_cache/vgg16_weights_th_dim_ordering_th_kernels.h5 --query="cooking vegetables"
+
+python cache_lime.py --word_index=/media/drive1/cwa5kor/personal/devise_cache/DICT_word_index.VAL.pkl --model=/media/drive1/cwa5kor/personal/devise_cache/epoch_13.hdf5 --vgg16=/media/drive1/cwa5kor/personal/devise_cache/vgg16_weights_th_dim_ordering_th_kernels.h5 --query="cutting vegetables"
+
+python cache_lime.py --word_index=/media/drive1/cwa5kor/personal/devise_cache/DICT_word_index.VAL.pkl --model=/media/drive1/cwa5kor/personal/devise_cache/epoch_13.hdf5 --vgg16=/media/drive1/cwa5kor/personal/devise_cache/vgg16_weights_th_dim_ordering_th_kernels.h5 --query="buying fruits"
+
+python cache_lime.py --word_index=/media/drive1/cwa5kor/personal/devise_cache/DICT_word_index.VAL.pkl --model=/media/drive1/cwa5kor/personal/devise_cache/epoch_13.hdf5 --vgg16=/media/drive1/cwa5kor/personal/devise_cache/vgg16_weights_th_dim_ordering_th_kernels.h5 --query="eating pizza at home"
+
+python cache_lime.py --word_index=/media/drive1/cwa5kor/personal/devise_cache/DICT_word_index.VAL.pkl --model=/media/drive1/cwa5kor/personal/devise_cache/epoch_13.hdf5 --vgg16=/media/drive1/cwa5kor/personal/devise_cache/vgg16_weights_th_dim_ordering_th_kernels.h5 --query="people walking at the beach"
+
